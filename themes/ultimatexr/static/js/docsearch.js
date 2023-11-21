53ef76d6e4fa34ccf72af141ac21e3e812a2b4e4
Paulo Tromp <paulo.tromp@vrmada.com>
2023-11-21 21:32:06 +0100

    Add DocSearch by Algolia

---
+++ themes/ultimatexr/static/js/docsearch.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  docsearch({
+    container: '#docsearch',
+    appId: 'ES5EXUJLVC',
+    indexName: 'ultimatexr',
+    apiKey: '67ef4db06aff6a97ecbf027dce191d6c',
+  });
+});
