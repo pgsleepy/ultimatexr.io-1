ed4f3ffcccb6e594e7985a42ae8658fa4dcc1e6e
Paulo Tromp <paulo.tromp@vrmada.com>
2023-11-22 20:28:25 +0100

    Tweak DocSearch styles

--- themes/ultimatexr/static/js/docsearch.js
+++ themes/ultimatexr/static/js/docsearch.js
@@ -1,8 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
   docsearch({
-    container: '#docsearch',
-    appId: 'ES5EXUJLVC',
-    indexName: 'ultimatexr',
-    apiKey: '67ef4db06aff6a97ecbf027dce191d6c',
+    container: "#docsearch",
+    appId: "ES5EXUJLVC",
+    indexName: "ultimatexr",
+    apiKey: "67ef4db06aff6a97ecbf027dce191d6c",
+    placeholder: "Search the docs…",
+    maxResultsPerGroup: 7
   });
 });
